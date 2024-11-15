f94c132ee51097d2fb0c4f82b21226e0aa579fcb
Hanif <amrhanif15@gmail.com>
2024-11-16 04:22:05 +0700

    Final version?

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# TO DO 
-# - cek fungsi gate
-# - pake fungsi aqeelappend (untuk ganti fungsi append)
-
 # Program Auto Gate MRT
-# ...
+# Program sederhana berbasis command line yang menyerupai sistem auto gate MRT. 
 
 # Assumptions
 # - Jarak antar stasiun adalah 2km
@@ -11,129 +7,155 @@
 # - Artinya, saldo minimum (untuk menempuh seluruh stasiun, yaitu 12 stasiun) yang dibutuhkan adalah 12xRp800 = Rp9600
 
 # Algoritma
-stations = ["Lebak Bulus Grab", "Fatmawati", "Cipete Raya", "Haji Nawi", "Blok A", "Blok M BCA", "ASEAN", "Senayan", "Isnora Mandiri", "Bendungan Hilir", "Setiabudi Astra", "Dukuh Atas BNI", "Bundaran HI"]
+stations = ["Lebak Bulus Grab", "Fatmawati", "Cipete Raya", "Haji Nawi", "Blok A", "Blok M BCA", "ASEAN", "Senayan",
+                "Istora Mandiri", "Bendungan Hilir", "Setiabudi Astra", "Dukuh Atas BNI", "Bundaran HI"] #array stasiun mrt
+indeks = [161,198,162,165,190,199,196] #array nim TPB
 cards = []
 
 # fungsi pembantu
-def findIndex(array, find):
+def findIndex(array, find): #input array dan item, output index item tsb dalam array 
     index = 0
     for i in array:
         if find != i:
             index += 1
         else:
             return index
-def findCardIndex(cards, cardID):
+def findCardIndex(cards, cardID): #input array cards dan id card, output index id card tersebut dalam array cards.
     index = 0
     for i in cards:
         if cardID != i[0]:
             index += 1
         else:
             return index
-def aqeelappend(prevArray , obj):
-    y = [0] * (len(prevArray)+1)
-    for i in range(0, len(y)):
-        if i < len(prevArray):
-            y[i] = prevArray[i]
-        else:
-            y[i] = obj
-    return y
 
 # untuk memasukkan data id kartu dan saldo awal (check isValid)
-def card(cardID, initialBalance):
-    # ... (random atau pake NIM)
+def kartuvalid(nomor_unik): #menentukan apakah 3 digit pertama id kartu merupakan nim tpb
+    ada = False
+    for i in range(0, len(indeks)):
+        if nomor_unik == indeks[i]:
+            ada = True
+    return ada
 
-    indeks=[161,198,162,165,190,199,196]
-    ada=False
-    nomor_unik=cardID//100000
-    if len(str(cardID))!= 8:
-        print("Nomor Tidak Valid (Jumlah Digit Tidak Sesuai)")
+def cekidcard(cards, idcard):
+    ada = False
+    y = 0
+    for i in range(0, len(cards)):
+        if cards[i][0] == idcard:
+            ada = True
+            y = i
+    return [ada, y]
+
+def cekntopup(cards):
+    if len(cards) != 0:
+        print("\nID Card : Nominal")
+        for i in range(0, len(cards)):
+            print(f"{cards[i][0]} : {cards[i][1]}")
     else:
-        for i in range (len(indeks)):
-            if nomor_unik==indeks[i] and ada==False:
-                ada=True
-            if ada==True and nomor_unik!=indeks[i]:
-                ada=True
-        if ada==False:
-            return [False, 'Nomor Tidak Valid (3 Digit Awal Wajib Merupakan NIM Jurusan Dengan Awalan "Sekolah")']
-        if ada==True and initialBalance<14000:
-            return [False, f"Saldo Anda Tidak Cukup (Dibutuhkan Saldo Minimum Rp14000)"]
-        elif ada==True and initialBalance>=14000:
-            cards.append([cardID, initialBalance]) # ganti pake aqeel append.
-            return [True, f"Silahkan Melanjutkan Perjalanan"]        
+        print("\nBelum ada kartu yang terdaftar.")
+    ingintopup = input("\nApakah anda ingin topup/meregistrasi kartu? (y/n): ")
+    if ingintopup == "n":
+        pass
+    else:
+        while True:
+            idcard = int(input("\nMohon masukan nomor ID Card: "))
+            if len(str(idcard)) != 8:
+                print("\nNomor Tidak Valid (Jumlah Digit Tidak Sesuai)")
+            else:
+                if not kartuvalid(idcard // 100000):
+                    print('\nNomor Tidak Valid (3 Digit Awal Wajib Merupakan NIM Jurusan Dengan Awalan "Sekolah")')
+                else:
+                    TopUp = int(input("\nMasukan nominal Top Up: "))
+                    y = cekidcard(cards, idcard)
+                    if y[0]:
+                        cards[y[1]][1] += TopUp
+                        print(f"\nSaldo terbaru anda (ID {cards[y[1]][0]}) adalah {cards[y[1]][1]}")
+                    else:
+                        cards += [[idcard, TopUp]]
+                        print(f"\nSaldo terbaru anda (ID {cards[len(cards)-1][0]}) adalah {cards[len(cards)-1][1]}")
+                    break
 
-def cardReader(idCard):
+def cardReader(idCard, cards):
+    status = cekidcard(cards, idCard)
+    if status[0]:
+    # cek saldo
+        if cards[status[1]][1] >= 14000:
+            return [True, "\nKartu Valid Dan Memenuhi Saldo Minimum"], cards # kartu valid dan memiliki saldo minimum yang cukup
+        else:
+            return [False, "\nSaldo Kurang Dari Rp14000"], cards
+    else:
+        return [False, "\nKartu belum terdaftar"], cards
 
-    cardsCount = len(cards)
+def gate(idCard, cards):
+    startLocation = int(input("\nMasukkan nomor stasiun awal: "))
+    while startLocation < 1 or startLocation > 13:
+        print("\nInput tidak valid!")
+        startLocation = int(input("\nMasukkan Nomor Stasiun Awal"))
+    
+    print("\nSelamat menikmati layanan MRT :)\n")
 
-
-    for i in range(0, cardsCount):
-        if cards[i][0] == idCard:
-            # cek saldo
-            if cards[i][1] >= 14000:
-                return [True, "Kartu Valid Dan Memenuhi Saldo Minimum"] # kartu valid dan memiliki saldo minimum yang cukup
-            return [False, "Saldo Kurang Dari Rp14000"]
-    # Mendaftarkan kartu baru
-    print("\nKartu Anda Baru Akan Didaftarkan.")
-    initialBalance = int(input("Masukkan Saldo Awal Anda: "))
-    cardStatus = card(idCard, initialBalance)
-    return cardStatus    
-
-
-
-def gate(idCard):
-
-    # Buat list stasiun dari no 1-13.
-
-    # Nanti inputnya berdasarkan no 1-13.
-
-    # Buat pengecualian kondisi jika user input selain 1-13. (Jika input selain 1-13, maka beri tahu bahwa input stasiun tidak sesuai dan jalankan kembali loop)
-
-    startLocation = input("\nMasukkan Stasiun Awal: ")
-        
-    idCardExit = int(input("Mohon Masukkan ID Kartu E Money Anda Untuk Keluar: "))
-
+    idCardExit = int(input("Mohon masukkan ID kartu E-Money anda untuk keluar: "))
     while idCardExit != idCard:
-        print("\nKartu Tidak Terdaftar Pada Sistem")
+        print("\nKartu tidak terdaftar pada sistem")
+        idCardExit = int(input("Mohon masukkan ID kartu E-Money anda untuk keluar: "))
 
-        idCardExit = int(input("Mohon Masukkan ID Kartu E Money Anda Untuk Keluar: "))
+    endLocation = int(input("\nMasukkan nomor stasiun akhir: "))
+    
+    while endLocation < 1 or endLocation > 13 or endLocation == startLocation:
+        print("\nInput tidak valid!")
+        endLocation = int(input("\nMasukkan nomor stasiun akhir: "))
+    
+    price_per_km = 400
+    jarak_ditempuh = abs(endLocation-startLocation)*2 #asumsikan jarak antar stasiun 2 km
+    tarif_awal = 3200
 
-    endLocation = input("\nMasukkan Stasiun Akhir: ")
+    price= jarak_ditempuh*price_per_km + tarif_awal #menentukan tarif
+    cardIndex = findCardIndex(cards, idCardExit)#mencari index kartu di database
+    cards[cardIndex][1] -= price #mengurangi saldo kartu
+    
+    #penyajian data
+    print(f"\nAnda telah menempuh perjalan sejauh {jarak_ditempuh} km")
+    print(f"Total tarif perjalan anda: Rp.{price},00")
+    print(f"Saldo akhir kartu E-Money anda (ID {idCard}): Rp.{cards[cardIndex][1]},00")
+    print("\nTerima Kasih telah menggunakan layanan MRT")
+    print("Sampai jumpa di perjalanan berikutnya")
 
-    startIndex = findIndex(stations, startLocation)
-    endIndex = findIndex(stations, endLocation)
-
-    pricePer2KM = 800
-    price = (abs(startIndex-endIndex)*pricePer2KM) + 3200 # Rp4000 adalah tarif minimum
-
-    cardIndex = findCardIndex(cards, idCardExit)
-
-    cards[cardIndex][1] -= price
-
-    print(f"\nAnda telah menempuh perjalanan sejauh {abs(startIndex-endIndex)*2} KM")
-    print(f"Total Tarif Perjalanan Anda: Rp{price}")
-    print(f"Saldo Akhir Kartu E Money Anda (ID {idCard}): Rp{cards[cardIndex][1]}")
-    print("\nTerima Kasih Telah Menggunakan Layanan MRT")
-    print("Sampai Jumpa Di Perjalanan Berikutnya")
-
-def main():
-
+def main(cards):
     while True:
         print("\n==========================================")
-        inp = input("Selamat Datang\n\nMohon Pilih Untuk Melanjutkan:\n1. Masuk MRT\n(tekan selain 1 untuk keluar program)\n\n: ")
+        inp = input("\nSelamat Datang\n"
+                    "\nMohon Pilih Untuk Melanjutkan:"
+                    "\n1. Masuk MRT"
+                    "\n2. Cek Saldo dan Top Up Kartu"
+                    "\n(Ketik angka selain 1 atau 2 untuk keluar)\n\n: ")
         if inp == "1":
             # masuk
+            print("\nList Stasiun MRT:\n"
+                  "1. Lebak Bulus Grab\n"
+                  "2. Fatmawati\n"
+                  "3. Cipete Raya\n"
+                  "4. Haji Nawi\n"
+                  "5. Blok A\n"
+                  "6. Blok M BCA\n"
+                  "7. ASEAN\n"
+                  "8. Senayan\n"
+                  "9. Istora Mandiri\n"
+                  "10. Bendungan Hilir\n"
+                  "11. Setiabudi Astra\n"
+                  "12. Dukuh Atas BNI\n"
+                  "13. Bundaran HI")
             print("\nMohon Masukkan ID Kartu E Money Anda Untuk Masuk: ")
             idCard = int(input("ID Card: "))
 
-            cardStatus = cardReader(idCard)
-
+            cardStatus, cards = cardReader(idCard, cards)
             if cardStatus[0]:
                 # lanjut ke fungsi gate
-                gate(idCard)
+                gate(idCard, cards)
             else:
                 print(cardStatus[1])
+        elif inp == "2":
+            cekntopup(cards)
         else:
             print("\nTerima Kasih Telah Menggunakan Layanan MRT")
             break
 
-main()
+main(cards)
--- test.py
+++
@@ -1,4 +0,0 @@
-for i in range(0, 0):
-    print("halo")
-
-print("halo di luar")
